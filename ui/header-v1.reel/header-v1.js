40a0e22994b708ba000770a1dd8e917a91f955f3
Disorder316 <noreply>
2015-10-03 17:45:07 +0000

    Add component header-v1

---
+++ ui/header-v1.reel/header-v1.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/header-v1.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class HeaderV1
+ * @extends Component
+ */
+exports.HeaderV1 = Component.specialize(/** @lends HeaderV1# */ {
+    constructor: {
+        value: function HeaderV1() {
+            this.super();
+        }
+    }
+});
